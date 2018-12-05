719386ac452b2f1bbe1870028b18703e44818b74
Easy-wei <easy-we@outlook.com>
2018-12-05 21:30:50 +0800

    12.05

--- pre_slove.py
+++ pre_slove.py
@@ -1,26 +1,37 @@
 import pandas as pd
-import time 
+import time
+import matplotlib.pyplot as plt
 
 data = pd.read_csv(r"D:\data\201602.csv")
 
 
-del (data["Type"] , data["Energysid"],data["Expertsid"],data["compsid"],data["areaenergysid"],data['Equsid'],data['buildingsid'],data['Load_Time'])
+del (data["Type"], data["Energysid"], data["Expertsid"], data["compsid"],
+     data["areaenergysid"], data['Equsid'], data['buildingsid'], data['Load_Time'])
 
-data['struct_time'] = data.Data_Time.apply( lambda x: time.strptime(x,'%d/%m/%Y %H:%M:%S'))
-# 得到格式化的struct——time，为以后时间的变化做基础
-data['weekday'] = data.struct_time.apply(lambda x: x[6])#0代表周一同理得到其他的日子
-data["year_day"] = data.struct_time.apply(lambda x: x[7])# 一年的第几天
+data['struct_time'] = data.Data_Time.apply(
+    lambda x: time.strptime(x, '%d/%m/%Y %H:%M:%S'))
+# 得到格式化的struct_time，为以后时间的变化做基础
+data['weekday'] = data.struct_time.apply(lambda x: x[6])  # 0代表周一同理得到其他的日子
+data["year_day"] = data.struct_time.apply(lambda x: x[7])  # 一年的第几天
+data['month_day'] = data.struct_time.apply(lambda x: int(time.strftime('%m%d', x)))
 del(data['Data_Time'])
 
-def day_power(data):
-    k=[]
-    for i in data['year_day'].unique():
-        for j in data[data['year_day']==i]['Name'].unique():
-            #print (i,j,round(data[(data['year_day']==i) & (data['Name']==j)]['Value'].sum(),3))
-            k.append( round(data[(data['year_day']==i) & (data['Name']==j)]['Value'].sum(),3))
-    return k
 
-data = data.drop_duplicates(['Name','year_day']).reset_index(drop = True)
-data['day_power'] = day_power(data)
-print(data.head(10),'\n',data.tail(10))
-            """  """
+def split(data):
+    list_1 = []
+    for i in data['Name'].unique():
+        pd = data[data.Name.isin([i])].reset_index(drop=True)
+        pd_1 = pd.drop_duplicates(['month_day']) # 先以month_day为去重的标准，然后删除Value这一列
+        del (pd_1['Value'])
+        list_2 = []
+        for j in pd['month_day'].unique():
+            list_2.append(pd[pd.month_day.isin([j])].Value.sum())
+        pd_1['day_power'] = list_2 # 老是提示这句话用的不合要求，但是能用，我担心这里埋雷
+        list_1.append(pd_1.reset_index(drop=True))
+    return list_1
+
+for j in split(data):
+    j.plot(x='month_day',y = 'day_power')
+
+plt.show()
+
---
+++ weather.py
@@ -0,0 +1,5 @@
+import pandas as pd 
+import time
+
+data = pd.read_csv(r'D:\data\weather.csv')
+
